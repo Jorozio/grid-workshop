4c44ff2197f468ffe0340551e31aa0a215a92143
Jorozio <joe.taylor@student.norwichuni.ac.uk>
2024-01-30 11:14:25 +0000

    first steps

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let rows = 5;
+let cols = 5;
+let size = 50;
 
-function draw() {
-  background(220);
+function setup() {
+  createCanvas(800, 800);
+  background(30);
+
+  for(let i = 0; i < rows;i++){
+    for(let j = 0; j < cols; i++){
+      const red = random(255);
+      const green = random(255);
+      const blue = random(255);
+
+      const myColor = color(red,green,blue);
+      fill(myColor)
+      rect(i,j,size)
+    }
+  }
 }
